894542d66e3533c31036d7a4ed1e2e0f152fa404
Frics-Mate <frics.mate@gmail.com>
2023-03-09 18:24:33 +0100

    JS es CSS update
    - számok kiszűrése, és alapértelmezett értékek kiírása - számok esetén

--- 2023.03.09/js/sargaturo.js
+++ 2023.03.09/js/sargaturo.js
@@ -5,7 +5,14 @@ function MennyisegSzamolo() {
     let tej = Number(letszam) * 0.2;
     let tojas = Number(letszam) * 2;
     let kristalyCukor = Number(letszam) * 1;
-    document.querySelector("#tej").innerHTML = tej.toFixed(1);
-    document.querySelector("#tojas").innerHTML = tojas;
-    document.querySelector("#cukor").innerHTML = kristalyCukor;
+    if (letszam >= 1) {
+        document.querySelector("#tej").innerHTML = tej.toFixed(1);
+        document.querySelector("#tojas").innerHTML = tojas;
+        document.querySelector("#cukor").innerHTML = kristalyCukor;
+    }
+    else {
+        document.querySelector("#tej").innerHTML = "0.2";
+        document.querySelector("#tojas").innerHTML = "2";
+        document.querySelector("#cukor").innerHTML = "1";
+    }
 }
